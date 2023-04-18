8eb9f37b658e33a502752e486a22116cefb11493
Ayrancioglu <ahmet.ayrancioglu@kpn.com>
2023-04-18 10:35:08 +0200

    add if main

--- download_transformer.py
+++ download_transformer.py
@@ -1,20 +1,46 @@
+import argparse
+from pprint import pprint
 from transformers import (
     AutoModelForSequenceClassification,
     PreTrainedModel,
+    AutoModel,
     AutoTokenizer,
     PreTrainedTokenizerFast,
 )
 
-model_name = "distilbert-base-cased"
-model: PreTrainedModel = AutoModelForSequenceClassification.from_pretrained(
-    model_name,
-    num_labels=46,
-)
 
-tokenizer: PreTrainedTokenizerFast = AutoTokenizer.from_pretrained(
-    model_name,
-    use_fast=True,
-)
+def main(args):
+    model_name = args["model"]
+    model: PreTrainedModel = AutoModel.from_pretrained(
+        model_name,
+        num_labels=46,
+    )
 
-model.save_pretrained(model_name)
-tokenizer.save_pretrained(model_name)
+    tokenizer: PreTrainedTokenizerFast = AutoTokenizer.from_pretrained(
+        model_name,
+        use_fast=True,
+    )
+
+    model.save_pretrained(model_name)
+    tokenizer.save_pretrained(model_name)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--model",
+        type=str,
+        default="distilbert-base-cased",
+        choices=[
+            "distilbert-base-cased",
+            "distilbert-base-uncased-finetuned-sst-2-english",
+        ],
+        help="model",
+    )
+
+    args = parser.parse_args()
+    args_dict = vars(args)
+    pprint(args_dict)
+
+    main(args_dict)
