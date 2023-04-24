ac645846aeb32590ec7e1913efd5a25a9461e897
Ayrancioglu <ahmet.ayrancioglu@kpn.com>
2023-04-24 11:52:25 +0200

    remove cache clear

--- SequentialSentenceClassifier.py
+++ SequentialSentenceClassifier.py
@@ -82,9 +82,6 @@ class SequentialSentenceClassifier(nn.Module):
         # Process each dialogue in the input dialogues
         max_len_batch = max(len(dialogue) for dialogue in dialogues)
         for dialogue in dialogues:
-            # Clear GPU cache before processing each dialogue
-            torch.cuda.empty_cache()
-
             dialogue_logits = []
             dialogue_embeddings = []
 
